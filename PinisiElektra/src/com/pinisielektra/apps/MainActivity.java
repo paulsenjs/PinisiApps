a4f52f0082e2b276a60f66eaebe6fadcf00bcce7
paulsenjs <paulsenjs@paulsens-mbp>
2014-11-10 23:55:20 +0700

    splash screen and dashboard

--- PinisiElektra/src/com/pinisielektra/apps/MainActivity.java
+++ PinisiElektra/src/com/pinisielektra/apps/MainActivity.java
@@ -1,33 +1,69 @@
 package com.pinisielektra.apps;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.LinearLayout;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+	private Handler handler;
+	private LinearLayout linearButtonLogin;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
         
-        //test
+        linearButtonLogin = (LinearLayout) findViewById(R.id.linear_button);
+        
+		handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				goToDashboardScreen();
+			}
+
+		};
+        
+		if (isAlreadyLogin()) {
+			linearButtonLogin.setVisibility(View.GONE);
+			new SplashLauncher().start();		
+		}else{
+			linearButtonLogin.setVisibility(View.VISIBLE);
+		}
+		
     }
 
+    private void goToDashboardScreen() {
+    	finish();
+		startActivity(new Intent().setClass(this, DashboardActivity.class));
+    }
 
+    private boolean isAlreadyLogin() {
+    	return true;
+    }
+    
+    class SplashLauncher extends Thread {
+		public void run() {
+			try {
+				Thread.sleep(3000);
+				Message msg = new Message();
+				String textTochange = "OK";
+				msg.obj = textTochange;
+				handler.sendMessage(msg);
+			} catch (Exception e) {
+				Log.e(getClass().getName(), e.getMessage());
+			}
+		}
+	}
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -47,21 +83,4 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
 }

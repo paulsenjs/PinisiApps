b4b23fe3f2020a2b6a1fe973c7cfac9e1e2fcac1
paulsenjs <paulsenjs@paulsens-mbp>
2014-11-10 23:57:51 +0700

    add dashboard activity and fragment

---
+++ PinisiElektra/src/com/pinisielektra/apps/DashboardActivity.java
@@ -0,0 +1,22 @@
+package com.pinisielektra.apps;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class DashboardActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dashboard);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.dashboard, menu);
+		return true;
+	}
+
+}
---
+++ PinisiElektra/src/com/pinisielektra/apps/fragment/DashboardFragment.java
@@ -0,0 +1,24 @@
+package com.pinisielektra.apps.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pinisielektra.apps.R;
+
+public class DashboardFragment extends Fragment{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+		
+		return view;
+	}
+}
